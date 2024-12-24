b15bf376ed810c60dbbbaafaa1673610cd54f366
Sarwan Surchi <sarwan.h.najim@gmail.com>
2024-12-25 02:00:32 +0300

    add assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,8 @@ const withGuider = guider({
   // The location of your theme file, created in the next step
   themeConfig: './theme.config.tsx',
 });
-
-const debug = process.env.NODE_ENV !== "production";
  
+const debug = process.env.NODE_ENV !== "production";
 export default withGuider({
   // These are the normal Next.JS settings.
   // Check out Next.JS docs: https://nextjs.org/docs/app/api-reference/next-config-js
