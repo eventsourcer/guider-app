db987cf229f2e014cc9acb3ea8fae308f02f9416
Sarwan Surchi <sarwan.h.najim@gmail.com>
2024-12-25 01:19:32 +0300

    add assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,12 @@ const withGuider = guider({
   // The location of your theme file, created in the next step
   themeConfig: './theme.config.tsx',
 });
+
+const debug = process.env.NODE_ENV !== "production";
  
 export default withGuider({
   // These are the normal Next.JS settings.
   // Check out Next.JS docs: https://nextjs.org/docs/app/api-reference/next-config-js
   output: 'export',
+  assetPrefix: !debug ? 'https://anotherplanet-io.github.io/Next-React-Components/' : '',
 });
